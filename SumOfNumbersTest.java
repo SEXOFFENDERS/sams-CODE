5345a48e08d195da4dd891f5c4c2563923bef97d
notasexoffender <yesplz443@gmail.com>
2017-06-01 13:31:57 -0600

    HUMBURGER
    COMMIT TO MASTER OR TASTE MY MASTER

--- SumOfNumbersTest.java
+++ SumOfNumbersTest.java
@@ -1,2 +1,7 @@
 import junit.framework.TestCase;
+
+public class SumOfNumbersTest extends TestCase {
+
+
 		assertEquals("1 + 2 + 3 + 4 + 5 = 15", sn.getSum(1,5));
+		assertEquals("21 = 21", sn.getSum(21,21));
